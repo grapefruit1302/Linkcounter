b55266107fa0ead41bbb67400ae1c9fa962c6beb
grapefruit1302 <148016286+grapefruit1302@users.noreply.github.com>
2024-02-14 08:41:22 +0200

    Update billing_utils.py

--- billing_utils.py
+++ billing_utils.py
@@ -1,5 +1,18 @@
 def add_TD(start_time, region, host, notes):
     print("add TD")
+    print("start_time: ", start_time)
+    print("region: ", region)
+    print("host: ", host)
+    print("notes:", notes)
 
-def close_TD(trigger_id):
+    #region:
+    #TE - Ternopil
+    #TEO - Ternopil region
+    #CG - Chervonograd
+    #VV - Volodimyr
+
+def close_TD(solution_time, region, host):
     print("close TD")
+    print("solution_time: ", solution_time)
+    print("region: ", region)
+    print("host: ", host)
